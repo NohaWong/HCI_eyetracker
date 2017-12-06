9274f26cddba162ad2b7dd6423cbe0de15a1bba3
Noha Wong <Noha.wong@e.ujf-grenoble.fr>
2017-12-06 16:29:37 +0100

    maj point

--- HCI/point.cpp
+++ HCI/point.cpp
@@ -3,34 +3,34 @@
 Point::Point(float x, float y)
 {
     this->x=x;
-    this->y=y,
+    this->y=y;
 }
 
 Point Point::copy()
 {
-    return new Point(this->x,this->y);
+    return Point(x,y);
 }
 
 float Point::distance_to(Point pt)
 {
-    float dx = this->x-pt.x;
-    float dy = this->y-pt.y;
+    float dx = x-pt.x;
+    float dy = y-pt.y;
     return sqrt(pow(dx,2)+pow(dy,2));
 }
 
 float Point::per_product(Point p2)
 {
-    return this.x*this.y - p2.x*p2.y;
+    return x*y - p2.x*p2.y;
 }
 
 vector<float> Point::mediatrice(Point pt)
 {
     std::vector<float> droite;
-    float milieu = new Point((this->x+pt.x)/2,(this->y+pt.y)/2);
+    Point milieu = Point((x+pt.x)/2,(y+pt.y)/2);
 
-    droite.push_back(pt.x-this->x);
-    droite.push_back(pt.y-this->y);
-    droite.push_back(-(droite.at(1)*milieu.y + droite.at(0)*milieu.x));
+    droite.push_back(pt.x-x);
+    droite.push_back(pt.y-y);
+    droite.push_back(-(droite[1]*milieu.y + droite[0]*milieu.x));
 
     return droite;
 }
--- HCI/point.h
+++ HCI/point.h
@@ -4,15 +4,16 @@
 #include <math.h>
 #include <vector>
 
+using namespace std;
+
 class Point
 {
 public:
     Point(float x, float y);
-    Point Point::copy();
-    float Point::distance_to(Point pt);
-    float Point::per_product(Point p2);
-    vector<float> Point::mediatrice(Point pt);
-private:
+    Point copy();
+    float distance_to(Point pt);
+    float per_product(Point p2);
+    vector<float> mediatrice(Point pt);
     float x;
     float y;
 
